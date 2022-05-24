b852a06eeb78acff882578940abe1dc24b9b3406
KnazzVladimir <Maimdelm@mail.ru>
2022-05-24 18:25:05 +0500

    Add Bogus-more Libery

--- Dictionasolution/example1/Program.cs
+++ Dictionasolution/example1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Bogus;
 namespace example1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+           Dictionary<string,int> players = new Dictionary<string,int>();
+           
+        }
+
+        private static void AddUser (Dictionary<string,int> origin)
+        {
+            
         }
     }
 }
